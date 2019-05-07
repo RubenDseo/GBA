6102cb6fc3693eb75223ca276228aecb242f58dc
AdrisGS <37232910+AdrisGS@users.noreply.github.com>
2019-05-07 12:09:27 -0500

    Cambios-Martes-7

--- prantokon/assets/js/gba.js
+++ prantokon/assets/js/gba.js
@@ -2,10 +2,13 @@ $( document ).ready(function() {
     $(function(){
        
         $(window).scroll(function(){
-            if($(this).scrollTop()>50){              
+            if($(this).scrollTop()>10){              
                 $('#logo-R').attr("style", "width: 100%");
+                $('#logo-R').addClass("tam-logo");
+
             }else{
                 $('#logo-R').attr("style", "width: 170%");
+                $('#logo-R').addClass("tam-logo");
             }
         });
     
