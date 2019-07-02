751475ee2443b8460771c9841dd75425d0590446
RubenDseo <dseo.stweb@gmail.com>
2019-07-02 11:07:07 -0500

    PequeñasModifi

--- formulario.php
+++ formulario.php
@@ -2,18 +2,23 @@
   $nombre = $_POST['name'];
   $email = $_POST['email'];
   $asunto = $_POST['subject'];
+  $telefono = $_POST['telefono'];
+  $mensaje = $_POST['message'];
 
   $contenido="\n\n";
   $contenido.="\n Nombre: ".$nombre;
   $contenido.="\n Email: ".$email;
   $contenido.="\n Asunto: ".$asunto;
+  $contenido.="\n Telefono: ".$telefono;
+  $contenido.="\n Mensaje: ".$mensaje;
   
   $cabecera= "From: ".$nombre. "< ".$email. " >\r\n";
 
   if(mail('adriaana.05@gmail.com',$asunto, $contenido, $cabecera)){
-    echo "error";
-  }else{
     echo "¡Gracias por tu mensaje ".$nombre."! Nos pondremos en contacto contigo  ";
+
+  }else{
+    echo "error";
   }
 
 ?>
