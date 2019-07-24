f9ac26e6d0763b49896267df25027506b99e276d
AdrisGS <37232910+AdrisGS@users.noreply.github.com>
2019-07-24 13:04:41 -0500

    borrando lo que no sirve + seo + clean codigo

--- app/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use ReCaptcha\ReCaptcha;
-
-require_once 'vendor/autoload.php';
-
-$recaptcha = new ReCaptcha('6LfO6KEUAAAAAAR-QdcRJ_QiGF547ScqlsIS3iK5');
--- js/formulario.js
+++ js/formulario.js
@@ -1,7 +1,6 @@
 var respuestaCaptcha;
 
 var verifyCallback = function(response) {
-    //console.log("el captcha se verifico correctamente "+response);
     if(response !=0){
         document.getElementById("btnEnviar").innerHTML = "<button type='button' onclick='enviaForm()' class='appao-btn appao-btn2' style='color: white;'>Enviar</button>";
     }
@@ -16,13 +15,6 @@ var onloadCallback = function() {
 
 
 function enviaForm(){
-    /*var formulario = document.getElementById('formulario-contacto');
-    var datos = new FormData(formulario);
-    console.log(datos)
-    console.log(datos.get('name'))
-    console.log(datos.get('email'))
-    console.log(datos.get('subject'))
-    console.log(datos.get('message'))*/
     var validacionDatos=validarDatos();
     if(validacionDatos){
         var nombre = document.getElementById('nombre').value;
@@ -36,8 +28,6 @@ function enviaForm(){
             ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             ajax.send("name="+nombre+"&email="+emailId+"&subject="+asunto+"&telefono="+telefonoId+"&message="+mensajeF);
             ajax.onreadystatechange = function(){
-              //  console.log(ajax.readyState);
-              //  console.log(ajax.status);
                 if(ajax.readyState == 4 && ajax.status == 200){
                     alert("Formulario enviado correctamente");
                 }else{
@@ -45,16 +35,12 @@ function enviaForm(){
                 }
             }
 
-                //alert("Se envio el formulario correctamente!");
                 document.getElementById("formulario-contacto").reset();
                 grecaptcha.reset(respuestaCaptcha);
                 document.getElementById("btnEnviar").innerHTML = "<button type='button' class='appao-btn appao-btn2' style='color: white; background: rgba(48, 69, 131, 0.45);' disabled>Enviar</button>";
     }else{
 
     }
-
-   // console.log(validacionDatos);
-
 }
 
 function validarDatos() {
@@ -63,11 +49,8 @@ function validarDatos() {
     var telefonoId = document.getElementById('telefono').value;
     var asunto = document.getElementById('asunto').value;
 
-    //var telefonoRegex2 = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
     var telefonoRegex = /^(\(\+?\d{2,3}\)[\*|\s|\-|\.]?(([\d][\*|\s|\-|\.]?){6})(([\d][\s|\-|\.]?){2})?|(\+?[\d][\s|\-|\.]?){8}(([\d][\s|\-|\.]?){2}(([\d][\s|\-|\.]?){2})?)?)$/;
-    //var nameRegex2 = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
     var nameRegex = /^[A-Za-záéíóúÁÉÍÓÚ]{1}[A-Za-záéíóúÁÉÍÓÚ\s]+$/;
-   // console.log(telefonoRegex.test(telefonoId));
    
     if(nombre == null || nombre.length == 0 || !(nameRegex.test(nombre)) ) {
         alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
