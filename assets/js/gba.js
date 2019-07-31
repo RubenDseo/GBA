865eadae3a86820d34e1f374dc7352c833d02957
AdrisGS <37232910+AdrisGS@users.noreply.github.com>
2019-07-31 14:45:21 -0500

    version 4.0

--- assets/js/gba.js
+++ assets/js/gba.js
@@ -1,17 +1,53 @@
 $( document ).ready(function() {
     console.log("hola00");
+    var ventana_ancho = $(window).width();
+    console.log(ventana_ancho);
     $(function(){
-       
-        $(window).scroll(function(){
-            if($(this).scrollTop()>10){              
-                $('#logo-R').attr("style", "width: 100%");
-                $('#logo-R').addClass("tam-logo");
+        if(ventana_ancho >= 768 && ventana_ancho<991){
+            $('#logo-R').attr("style", "width: 140%");
 
-            }else{
-                $('#logo-R').attr("style", "width: 170%");
-                $('#logo-R').addClass("tam-logo");
-            }
-        });
+            $(window).scroll(function(){
+                if($(this).scrollTop()>10){              
+                    $('#logo-R').attr("style", "width: 100%");
+                    $('#logo-R').addClass("tam-logo");
+
+                }else{
+                    $('#logo-R').attr("style", "width: 140%");
+                    $('#logo-R').addClass("tam-logo");
+                }
+            });
+        }
+
+        if(ventana_ancho>=991){
+            $('#logo-R').attr("style", "width: 170%");
+
+            $(window).scroll(function(){
+                if($(this).scrollTop()>10){              
+                    $('#logo-R').attr("style", "width: 100%");
+                    $('#logo-R').addClass("tam-logo");
+
+                }else{
+                    $('#logo-R').attr("style", "width: 170%");
+                    $('#logo-R').addClass("tam-logo");
+                }
+            });
+        }
+
+        if(ventana_ancho<576){
+            $('#logo-R').attr("style", "width: 170%");
+
+            $(window).scroll(function(){
+                if($(this).scrollTop()>10){              
+                    $('#logo-R').attr("style", "width: 100%");
+                    $('#logo-R').addClass("tam-logo");
+
+                }else{
+                    $('#logo-R').attr("style", "width: 170%");
+                    $('#logo-R').addClass("tam-logo");
+                }
+            });
+        }
+        
     
 
     });
